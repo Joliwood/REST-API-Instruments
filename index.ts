59323f3fe9946920f5f0a487d1f7ae13fd6fc7ef
Guillaume <jolibois.contact@gmail.com>
2023-06-29 19:22:28 +0200

    :zap: Cors added

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
 import router from "./app/routers";
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
@@ -8,6 +9,8 @@ const PORT: string | number = process.env.PORT || 3000;
 const BASE_URL: string = process.env.BASE_URL || "localhost";
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(router);
 
